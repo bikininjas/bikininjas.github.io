d4969a23b466c4c8a76825f7953043e94dc5d6a4
Seb PikPik <sebpicot@gmail.com>
2025-03-04 06:43:01 +0100

    fix(vite): fix vite env

---
+++ src/vite-env.d.ts
@@ -0,0 +1,8 @@
+/// <reference types="vite/client" />
+
+interface ImportMeta {
+  env: {
+    VITE_API_URL: string; // Add any other environment variables you need
+    // Add more environment variables as needed
+  };
+}
