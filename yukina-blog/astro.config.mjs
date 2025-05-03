376c6fff478c8c87e8082dc76e56c48d5bb6fc22
Seb PikPik <sebpicot@gmail.com>
2025-05-03 15:49:32 +0200

    feat(package): add React and related dependencies for component-based UI feat(astro.config.mjs): integrate React support into Astro configuration refactor(Banner.astro): optimize carousel implementation and improve animation handling refactor(BaseHead.astro): clean up imports and ensure proper structure refactor(calendar.astro): switch layout component to MainLayout for consistency and clarity

--- yukina-blog/astro.config.mjs
+++ yukina-blog/astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import svelte from "@astrojs/svelte";
 import swup from "@swup/astro";
+import react from "@astrojs/react";
 
 import rehypeSlug from "rehype-slug";
 import rehypeKatex from "rehype-katex";
@@ -22,6 +23,7 @@ export default defineConfig({
   integrations: [
     tailwind(),
     svelte(),
+    react(),
     icon(),
     swup({
       theme: false,
